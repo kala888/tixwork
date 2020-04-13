c03f9d704af80ee0699eb7c19b19ab55da11b012
kala888 <kala.niubility@gmail.com>
2020-04-13 11:39:33 +0800

    refactor config

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,27 +2,22 @@ import viewConfig from './viewmapping.config'
 
 const baseURL = 'https://demo2.doublechaintech.com/storedev/wxappService/' //后端服务地址
 
-const niceRouterConfig = {
-  name: '双链科技',
+const Config = {
+  name: 'NiceRouter App Start',
   baseURL,
   version: 1,
   appType: 'mini-program',
   viewConfig,
   backendRouterPageKeyBlackList: ['refreshPage/', 'goBack/', 'goPrevious/'],
-  backendRouterPageBlackList: ['NetworkException', 'EditProfileForm', 'Login', 'Me', 'UserDetail'],
+  backendRouterPageBlackList: ['NetworkException'],
   api: {
     FooterHome: 'viewHomepage/',
     FooterMe: 'customerViewDashboard/',
+
     Login: 'clientLogin/',
     VerifyCode: 'sendVerifyCode/:mobile/',
     OSSToken: 'customGetOssToken/',
   },
-}
-
-const Config = {
-  ...niceRouterConfig,
-  name: 'NiceRouter App Start',
-  goBackSkipScreen: [],
   // loginMethod:'wechat',
   // loginMethod:'vcode',
   loginMethod: 'password',
