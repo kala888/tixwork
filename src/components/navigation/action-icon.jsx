8a64ac197a66a52517474502ead5c551fc638b8f
kala888 <kala.niubility@gmail.com>
2020-05-11 17:45:16 +0800

    change AtICON to iconfont

--- src/components/navigation/action-icon.jsx
+++ src/components/navigation/action-icon.jsx
@@ -2,16 +2,24 @@ import { isNotEmpty } from '@/nice-router/nice-router-util'
 import ServerImage from '@/server-image/server-image'
 import { View } from '@tarojs/components'
 import classNames from 'classnames'
-import { AtIcon } from 'taro-ui'
 
 import './styles.scss'
 
+/**
+ *  有icon，优先展示ICON
+ * @param icon
+ * @param imageUrl
+ * @param className
+ * @param mode
+ * @returns {*}
+ * @constructor
+ */
 function ActionIcon({ icon, imageUrl, className, mode }) {
   const rootClass = classNames('action-icon', className)
   return (
     <View className={rootClass}>
-      {isNotEmpty(icon) ? (
-        <AtIcon className={rootClass} prefixClass='iconfont' value={icon} />
+      {isNotEmpty(imageUrl) ? (
+        <View className={`iconfont ${icon}`} />
       ) : (
         <ServerImage customStyle={{ width: '100%', height: '100%' }} mode={mode} src={imageUrl} />
       )}
