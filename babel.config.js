fbb25f4ed344809678a4edb0aeb1e27fa620534a
kala888 <kala.niubility@gmail.com>
2020-05-19 17:14:04 +0800

    修改lodash的压缩

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,12 @@
 // babel-preset-taro 更多选项和默认值：
 // https://github.com/NervJS/taro/blob/next/packages/babel-preset-taro/README.md
 module.exports = {
+  plugins: ["lodash"],
   presets: [
     ['taro', {
       framework: 'react',
       ts: false
-    }]
+    }],
+    ["@babel/env", { "targets": { "node": 6 } }]
   ]
 }
--- config/index.js
+++ config/index.js
@@ -80,6 +80,9 @@ const config = {
     webpackChain(chain) { // 将 lodash 单独拆分出来 (防止vendors.js过大)
       // chain.plugin('analyzer')
       //   .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin, [])
+
+      chain.plugin('lodash-webpack-plugin').use(require('lodash-webpack-plugin'))
+
       chain.merge({
         optimization: {
           splitChunks: {
