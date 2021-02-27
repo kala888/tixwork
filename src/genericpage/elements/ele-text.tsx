34b3092ce25be3fa0af350cba838c316d986ef45
kala888 <kala.niubility@gmail.com>
2021-02-27 14:49:49 +0800

    add customStyle to text

--- src/genericpage/elements/ele-text.tsx
+++ src/genericpage/elements/ele-text.tsx
@@ -1,20 +1,22 @@
-import NavigationService from '@/nice-router/navigation-service';
-import { Text } from '@tarojs/components';
-import classNames from 'classnames';
-import { ActionLike } from '@/nice-router/nice-router-types';
+import React from "react";
+import NavigationService from "@/nice-router/navigation-service";
+import { Text } from "@tarojs/components";
+import classNames from "classnames";
+import { ActionLike } from "@/nice-router/nice-router-types";
 
 type EleTextProps = {
   text: string;
   className?: string;
+  customStyle?: React.CSSProperties;
 } & ActionLike;
 
 function EleText(props: EleTextProps) {
-  const { text, className } = props;
+  const { text, className, customStyle } = props;
   const onClick = () => NavigationService.view(props);
 
-  const rootClass = classNames('ele-text', className);
+  const rootClass = classNames("ele-text", className);
   return (
-    <Text className={rootClass} onClick={onClick}>
+    <Text className={rootClass} onClick={onClick} style={customStyle}>
       {text}
     </Text>
   );
