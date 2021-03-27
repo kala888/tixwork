059ea3525094f893146667b5380f776c32894a50
kala888 <kala.niubility@gmail.com>
2021-03-27 14:50:20 +0800

    #fixed #16 修改ele-convas

--- src/components/elements/ele-qrcode.tsx
+++ src/components/elements/ele-qrcode.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { toRpx } from '@/utils/index';
 import { Canvas } from '@tarojs/components';
-import Taro from '@tarojs/taro';
+import Taro, { useReady } from '@tarojs/taro';
 import _ from 'lodash';
 import ErrorCorrectLevel from 'qr.js/lib/ErrorCorrectLevel';
 import QRCodeImpl from 'qr.js/lib/QRCode';
 
 // 修改自 https://github.com/xueyida/QRcode.taro/blob/master/QRCodeCanvas.js
-const defaultCanvasId = 'qrcode-canvase';
+const defaultCanvasId = 'qrcode-canvas';
 
 type EleQrcodeProps = {
   size: number;
@@ -20,59 +20,43 @@ type EleQrcodeProps = {
   customStyle?: React.CSSProperties;
 };
 
-export default class EleQrcode extends React.PureComponent<EleQrcodeProps> {
-  static defaultProps = {
-    size: 200,
-    level: 'L',
-    bgColor: '#fff',
-    fgColor: '#000000',
-    logoSize: 100,
-    logo: null,
-    text: 'https://www.github.com/kala888',
-  };
-
-  componentDidMount() {
-    this.update();
-  }
-
-  componentDidUpdate() {
-    this.update();
-  }
-
-  convertStr(str) {
-    let out = '';
-    for (let i = 0; i < str.length; i++) {
-      let charcode = str.charCodeAt(i);
-      if (charcode < 0x0080) {
-        out += String.fromCharCode(charcode);
-      } else if (charcode < 0x0800) {
-        out += String.fromCharCode(0xc0 | (charcode >> 6));
-        out += String.fromCharCode(0x80 | (charcode & 0x3f));
-      } else if (charcode < 0xd800 || charcode >= 0xe000) {
-        out += String.fromCharCode(0xe0 | (charcode >> 12));
-        out += String.fromCharCode(0x80 | ((charcode >> 6) & 0x3f));
-        out += String.fromCharCode(0x80 | (charcode & 0x3f));
-      } else {
-        // This is a surrogate pair, so we'll reconsitute the pieces and work
-        // from that
-        i++;
-        charcode = 0x10000 + (((charcode & 0x3ff) << 10) | (str.charCodeAt(i) & 0x3ff));
-        out += String.fromCharCode(0xf0 | (charcode >> 18));
-        out += String.fromCharCode(0x80 | ((charcode >> 12) & 0x3f));
-        out += String.fromCharCode(0x80 | ((charcode >> 6) & 0x3f));
-        out += String.fromCharCode(0x80 | (charcode & 0x3f));
-      }
+const convertStr = (str) => {
+  let out = '';
+  for (let i = 0; i < str.length; i++) {
+    let charCode = str.charCodeAt(i);
+    if (charCode < 0x0080) {
+      out += String.fromCharCode(charCode);
+    } else if (charCode < 0x0800) {
+      out += String.fromCharCode(0xc0 | (charCode >> 6));
+      out += String.fromCharCode(0x80 | (charCode & 0x3f));
+    } else if (charCode < 0xd800 || charCode >= 0xe000) {
+      out += String.fromCharCode(0xe0 | (charCode >> 12));
+      out += String.fromCharCode(0x80 | ((charCode >> 6) & 0x3f));
+      out += String.fromCharCode(0x80 | (charCode & 0x3f));
+    } else {
+      // This is a surrogate pair, so we'll reconsitute the pieces and work
+      // from that
+      i++;
+      charCode = 0x10000 + (((charCode & 0x3ff) << 10) | (str.charCodeAt(i) & 0x3ff));
+      out += String.fromCharCode(0xf0 | (charCode >> 18));
+      out += String.fromCharCode(0x80 | ((charCode >> 12) & 0x3f));
+      out += String.fromCharCode(0x80 | ((charCode >> 6) & 0x3f));
+      out += String.fromCharCode(0x80 | (charCode & 0x3f));
     }
-    return out;
   }
+  return out;
+};
 
-  update() {
-    const { text, size, level, bgColor, fgColor, logoSize = '100', logo = '' } = this.props;
+function EleQrcode(props: EleQrcodeProps) {
+  const { text, size, level, bgColor, fgColor, logoSize = '100', logo = '', customStyle = {} } = props;
+
+  const update = () => {
     const qrcode = new QRCodeImpl(-1, ErrorCorrectLevel[level]);
-    qrcode.addData(this.convertStr(text));
+    qrcode.addData(convertStr(text));
     qrcode.make();
     // @ts-ignore
-    const canvas = Taro.createCanvasContext(defaultCanvasId, this.$scope);
+    const canvas = Taro.createCanvasContext(defaultCanvasId);
+    console.log('current', canvas);
     if (!canvas) {
       return;
     }
@@ -102,13 +86,24 @@ export default class EleQrcode extends React.PureComponent<EleQrcodeProps> {
       canvas.drawImage(logo, (size - logoSize) / 2, (size - logoSize) / 2, logoSize, logoSize);
     }
     canvas.draw();
-  }
+    // });
+  };
+  // useReady(() => Taro.nextTick(() => setTimeout(update, 100)));
+  useEffect(() => Taro.nextTick(() => setTimeout(update, 100)));
 
-  render() {
-    // style:{marginTop: '400px'}
-    const { size, customStyle = {} } = this.props;
-    const itemWith = toRpx(size);
-    const canvasStyle = { height: itemWith, width: itemWith, ...customStyle };
-    return <Canvas canvasId={defaultCanvasId} style={canvasStyle} />;
-  }
+  const itemWith = toRpx(size);
+  const canvasStyle = { height: itemWith, width: itemWith, ...customStyle };
+  return <Canvas id={defaultCanvasId} canvasId={defaultCanvasId} style={canvasStyle} />;
 }
+
+EleQrcode.defaultProps = {
+  size: 200,
+  level: 'L',
+  bgColor: '#fff',
+  fgColor: '#000000',
+  logoSize: 100,
+  logo: null,
+  text: 'https://www.github.com/kala888',
+};
+
+export default EleQrcode;
