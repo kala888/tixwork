4ce78966bd74d80d7b5db5b30cfe76b845f0b2fc
ellischen@aaxiscommerce.com <2020qiuQIU>
2024-02-26 15:32:18 +0800

    adjust width of UI

--- tixwork-ui/src/components/grouped-checkbox/field.tsx
+++ tixwork-ui/src/components/grouped-checkbox/field.tsx
@@ -48,7 +48,7 @@ export default (props: GroupedCheckboxFieldType) => {
     return {
       width: '100%',
       '.ant-checkbox-wrapper': {
-        minWidth: 100,
+        minWidth: 110,
         color: token.colorTextLabel,
       },
     };
